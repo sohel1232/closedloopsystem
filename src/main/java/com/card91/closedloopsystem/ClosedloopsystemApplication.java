09b3e6af941b3cc0d531f8ee9a9cadbeb5f74840
sohel khan <m.sohelkhan2324516@gmail.com>
2024-04-24 12:41:47 +0530

    project initialised

---
+++ src/main/java/com/card91/closedloopsystem/ClosedloopsystemApplication.java
@@ -0,0 +1,13 @@
+package com.card91.closedloopsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosedloopsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosedloopsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/card91/closedloopsystem/ClosedloopsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.card91.closedloopsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosedloopsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
